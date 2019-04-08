e0075f613b58081909d31a862df58a8445a9572f
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-04-08 19:06:00 -0400

    can now get a random character

--- public/app.js
+++ public/app.js
@@ -1,4 +1,18 @@
 const app = angular.module('GameApp',[]);
 app.controller('GameController',['$http', function ($http) {
-  
+  const game = this;
+
+  this.getCharacterInfo = function () {
+    const num = Math.floor(Math.random() * 44360) + 1
+    $http({
+      method:"GET",
+      url:"https://api.jikan.moe/v3/character/" + num
+    }).then(function (res) {
+      console.log(res.data);
+      console.log(num);
+      game.character = res.data
+    },function (err) {
+      console.log(err);
+    })
+  }
 }])
